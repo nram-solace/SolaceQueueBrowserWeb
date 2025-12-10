9eb8d2c94275c5d7bdd6eb1d9e46f9633e6af841
Ramesh Natarajan <ramesh.natarajan@solace.com>
2025-12-10 16:55:18 -0600

    Fix version info

--- src/config/version.js
+++ src/config/version.js
@@ -1,7 +1,7 @@
 // Version configuration - update this file to change version without code changes
-export const APP_VERSION = '2.0.9';
-export const APP_NAME = 'SolaceQueueBrowserWeb2.0';
+export const APP_VERSION = '2.0.10';
+export const APP_NAME = 'SolQBrowser/Web';
 
-export const APP_TITLE = `${APP_NAME} - v${APP_VERSION}`;
-export const WINDOW_TITLE = 'SolQBrowserWeb2.0';
+export const APP_TITLE = `${APP_NAME}/v${APP_VERSION}`;
+export const WINDOW_TITLE = 'SolQBrowser/Web/2.0';
 
